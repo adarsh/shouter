00adf5afb42dce623eda1b895dd142469742d79a
Matthew Mongeau <halogenandtoast@gmail.com>
2012-03-12 16:28:19 -0400

    Show users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Shouter::Application.routes.draw do
   root to: "welcome#index"
   resource :dashboard, only: [:show]
+  resources :users, only: [:show]
   resources :shouts, only: [:show]
   resources :text_shouts, only: [:create]
   resources :photo_shouts, only: [:create]
