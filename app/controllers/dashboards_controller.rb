89adc79c5a6f245385ecd87120c61da5c786ee21
Matthew Mongeau <halogenandtoast@gmail.com>
2012-03-12 17:15:06 -0400

    Eager load

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,6 @@ class DashboardsController < ApplicationController
   def show
     @text_shout = TextShout.new
     @photo_shout = PhotoShout.new
-    @shouts = current_user.shouts.current
+    @shouts = current_user.shouts.current.includes(:medium => :shout)
   end
 end
