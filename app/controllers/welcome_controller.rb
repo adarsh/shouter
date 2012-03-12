621ad8d016caf895d6c036224690406f872ecd57
Matthew Mongeau <halogenandtoast@gmail.com>
2012-03-12 10:23:21 -0400

    Dashboard redirect.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,8 @@
 class WelcomeController < ApplicationController
+  before_filter :redirect_to_dashboard_if_signed_in
+
+  private
+  def redirect_to_dashboard_if_signed_in
+    redirect_to dashboard_path if signed_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Shouter::Application.routes.draw do
   root to: "welcome#index"
+  resource :dashboard
 end
